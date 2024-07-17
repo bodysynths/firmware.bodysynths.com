4a8803012a6cbac01c20dd078186d02f45e56244
Isa <isa@symbios.wiki>
2024-07-17 14:35:31 +0200

    Deploy main

--- next.config.mjs
+++ next.config.mjs
@@ -5,6 +5,8 @@ const isProd = process.env.NODE_ENV === 'production'
 const nextConfig = {
   distDir: 'build',
   output: 'export',
+  // assetPrefix: isProd ? '/static/' : undefined,
+  basePath: isProd ? '/body-synths-firmare' : undefined,
 }
 
 export default nextConfig
