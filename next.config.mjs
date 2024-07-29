940053b8e2a806672840c1c416af7ae069edd099
Isa <isa@symbios.wiki>
2024-07-29 16:14:02 +0200

    Deploy main

--- next.config.mjs
+++ next.config.mjs
@@ -10,6 +10,9 @@ const nextConfig = {
   output: "export",
   assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH,
   basePath: process.env.NEXT_PUBLIC_BASE_PATH,
+  env: {
+    NEXT_PUBLIC_BASE_PATH: process.env.NEXT_PUBLIC_BASE_PATH,
+  },
 };
 
 export default nextConfig;
