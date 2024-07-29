bad138bf0952a89dc1da0b30d785904badeec077
Isa <isa@symbios.wiki>
2024-07-29 16:18:45 +0200

    Deploy main

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,9 @@
 /** @type {import('next').NextConfig} */
 
-const isProd = process.env.NODE_ENV === "production";
-const basePath = isProd ? "/body-synths-firmware" : "";
+// const isProd = process.env.NODE_ENV === "production";
+// const basePath = isProd ? "/body-synths-firmware" : "";
 
-console.log("NEXT_PUBLIC_BASE_PATH: ", process.env.NEXT_PUBLIC_BASE_PATH);
+// console.log("NEXT_PUBLIC_BASE_PATH: ", process.env.NEXT_PUBLIC_BASE_PATH);
 
 const nextConfig = {
   distDir: "build",
@@ -12,6 +12,16 @@ const nextConfig = {
   basePath: process.env.NEXT_PUBLIC_BASE_PATH,
   env: {
     NEXT_PUBLIC_BASE_PATH: process.env.NEXT_PUBLIC_BASE_PATH,
+  },
+
+  webpack(config, { isServer }) {
+    if (!isServer) {
+      console.log(
+        "NEXT_PUBLIC_BASE_PATH from next.config.js: ",
+        process.env.NEXT_PUBLIC_BASE_PATH
+      );
+    }
+    return config;
   },
 };
 
