7ac2c1432150a1d92b9d4484b59b023a149cd797
Isa <isa@symbios.wiki>
2024-07-29 16:12:25 +0200

    Deploy main

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,8 @@
 const isProd = process.env.NODE_ENV === "production";
 const basePath = isProd ? "/body-synths-firmware" : "";
 
+console.log("NEXT_PUBLIC_BASE_PATH: ", process.env.NEXT_PUBLIC_BASE_PATH);
+
 const nextConfig = {
   distDir: "build",
   output: "export",
